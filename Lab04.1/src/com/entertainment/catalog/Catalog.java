f91b66eecc935ee4fc1bcf8aff329f12a3fd2e7f
Tomiwa Akinbayo <tomiwaa@tlgcohort.com>
2024-06-07 13:34:14 -0500

    Implemented findByBrands and Unit tests

--- Lab04.1/src/com/entertainment/catalog/Catalog.java
+++ Lab04.1/src/com/entertainment/catalog/Catalog.java
@@ -28,7 +28,14 @@ public class Catalog {
      * A no-matches result should be an empty collection (not null).
      */
     public static Collection<Television> findByBrand(String brand) {
-        return null;
+        Collection<Television> result = new ArrayList<>();
+
+        for (Television tv: catalog){
+            if(tv.getBrand().equals(brand)){
+                result.add(tv);
+            }
+        }
+        return result;
     }
 
     /**
@@ -37,7 +44,12 @@ public class Catalog {
      * A no-brands-passed result should be an empty map (not null).
      */
     public static Map<String,Collection<Television>> findByBrands(String... brands) {
-        return null;
+        Map<String,Collection<Television>> result = new HashMap<>();
+
+        for(String brand: brands) {
+            result.put(brand, findByBrand(brand));
+        }
+        return result;
     }
 
     /**
