3278e8c5c00798a23d6fdaa12345bc6b30b02504
Tomiwa Akinbayo <tomiwaa@tlgcohort.com>
2024-06-13 13:36:45 -0500

    Sort List<radish> via lambda as Comparator

---
+++ Scratch/src/com/crisp/RadishSortTestLambda.java
@@ -0,0 +1,44 @@
+package com.crisp;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+class RadishSortTestLambda {
+    public static void main(String[] args) {
+        List<Radish> radishes = new ArrayList<>();
+
+        radishes.add(new Radish("white", 1.5, 2.5, 5));
+        radishes.add(new Radish("black", 1.0, 0.0, 0));
+        radishes.add(new Radish("red", 2.7, 5.5, 0));
+        radishes.add(new Radish("pink", 0.9, 0.0, 6));
+
+        System.out.println("Original List");
+        dump(radishes);
+
+        System.out.println("Sorted by Natural order");
+        radishes.sort(null);    // passing null means natural order
+        dump(radishes);
+
+        System.out.println("Sort by color via lambda as the Comparator");
+        radishes.sort((r1, r2) -> r1.getColor().compareTo(r2.getColor()));
+        dump(radishes);
+
+        System.out.println("Sort by guysOnTop lambda as the Comparator");
+        radishes.sort((r1, r2) -> Integer.compare(r1.getGuysOnTop(), r2.getGuysOnTop()));
+        dump(radishes);
+
+        System.out.println("sort by tailLength via lambda as the Comparator");
+        radishes.sort((r1, r2) -> Double.compare(r1.getTailLength(), r2.getTailLength()));
+        dump(radishes);
+    }
+
+    private static void dump(List<Radish> radishes) {
+        for(Radish radish : radishes) {
+            System.out.println(radish);
+        }
+        System.out.println();
+    }
+
+
+}
